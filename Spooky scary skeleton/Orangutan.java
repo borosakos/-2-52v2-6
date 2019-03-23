82d1829af7d170d4c3e6f9df1571d213420370e6
Ferenczy Bálint <balint.ferenczy@gmail.com>
2019-03-23 21:00:20 +0100

    Controller, Orangutan, Question kommetelése

--- Spooky scary skeleton/Orangutan.java
+++ Spooky scary skeleton/Orangutan.java
@@ -1,30 +1,48 @@
 package skeleton;
 
-
-
+/**
+* Az orangutan osztaly, ami az Animal osztalybol szarmazik, igy heterogen kollekcioban
+* tarolhato egyutt mas allatokkal.
+* Ezt fogja iranyitani a jatekos, aki mas jatekosokkal ellenben haromszot lephet egy korben.
+*
+* @author		Ferenczy Balint
+* @version 	1.0
+*/
 public class Orangutan extends Animal {
-	
+
+	/**
+	* Tarolja a jatekos pontszamait, amiket a pandak utan kap.
+	**/
 	int points;
 
+	/**
+	* Elengedi a mogotte allo panda kezet, ha van olyan.
+	**/
 	@Override
 	public void release() {
 		Indent.print("Orangutan release()");
-		
+		if(backNeighbour!=null){backNeighbour.release()};
 	}
 
+	/**
+	* Megoli az orangutant, ezzel egyutt befejezi a jatekot.
+	**/
 	@Override
 	public void die() {
 		Indent.print("Orangutan die()");
 		Indent.inc();
-		
+
 		if (backNeighbour!=null) {
 			backNeighbour.release();
 		}
 		Controller.game.endGame();
-		
 		Indent.dec();
 	}
-
+	/**
+	* Kivalasztja, milyen csempere lepjen az otangutan.
+	* Ehhez a selectTileQuestions fuggveny kerdeseit hivja segitsegul
+	* @return Visszater a valasztott csempevel
+	**/
 	@Override
 	public Tile selectTile() {
 		Indent.print("Orangutan selectTile()");
@@ -34,12 +52,24 @@ public class Orangutan extends Animal {
 		return t;
 	}
 
+	/**
+	* Kezeli, mi tortenik, amikor az orangutannak nekimeg egy masik orangutan.
+	* Ertelemszeruen ezt a lepest nem engedelyezi
+	* @param o Az orangutan, ami nekiutkozik
+	* @return mindig hamissal ter vissza, hisz olyan mezore nem lephet, amin all orangutan
+	**/
 	@Override
 	public boolean hitBy(Orangutan o) {
 		Indent.print("Orangutan hitBy(Orangutan)");
 		return false;
 	}
 
+	/**
+	* Kezeli, mi tortenik, amikor az orangutannak nekimegy egy panda.
+	* Ertelemszeruen ezt a lepest nem engedelyezi
+	* @param p A panda, ami nekiutkozik az orangutannak
+	* @return mindig hamissal ter vissza, hisz olyan mezore nem lephet, amin all orangutan
+	**/
 	@Override
 	public boolean hitBy(Panda p) {
 		// TODO Auto-generated method stub
@@ -47,6 +77,12 @@ public class Orangutan extends Animal {
 		return false;
 	}
 
+	/**
+	* Atadja azt a mezo elemet, amire ra akar lepni az orangutan.
+	* Erre meghivodik az utkozes.
+	* @param e Az az elem, ami a valasztott csempen van
+	* @return visszaadja az utkozes eredmenyet, azaz, hogy ralephet-e a csempere
+	**/
 	@Override
 	public boolean collideWith(Element e) {
 		Indent.print("Orangutan collideWith(Element)");
@@ -56,6 +92,11 @@ public class Orangutan extends Animal {
 		return res;
 	}
 
+	/**
+	* Az orangutan lepes fuggvenye.
+	* Ebben kivalasztja, melyik mezore lep, utkozik a benne levo objektumnak,
+	* es az utkozes eredmenyenek megfeleloen kezeli a kovetkezmenyeket.
+	**/
 	@Override
 	public void step() {
 		// TODO Auto-generated method stub
@@ -74,7 +115,12 @@ public class Orangutan extends Animal {
 			}
 		Indent.dec();
 	}
-	
+
+	/**
+	* Ellenorzi, sorban all-e a panda.
+	* Itt lehet kivalasztani, alljanak-e utana pandak.
+	* @return Igaz, ha all mogotte panda. Hamis, ha nem.
+	**/
 	@Override
 	public boolean isInQueue() {
 		Indent.print("Orangutan isInQueue()");
@@ -86,6 +132,12 @@ public class Orangutan extends Animal {
 		return (backNeighbour!=null);
 	}
 
+	/**
+	* Megszamolja a mogotte sorban allo pandak utan, hogy mennyi pont
+	* jar a jatekosnak. Otnel tobb panda eseten +5, 10-nel tobb panda
+	* eseten +10 pont jar.
+	* @return visszaadja pontok szamat
+	**/
 	public int countPoints() {
 		Indent.print("Orangutan countPoints()");
 		if(5<=points && points<10) {
@@ -96,13 +148,20 @@ public class Orangutan extends Animal {
 		return points;
 	}
 
+	/**
+	* A felvett pandak utan hozzaadodik egy pont a jatekosnak.
+	* @param incr hozzaad egy pontot a jatekos pontjaihoz
+	**/
 	public void addPoints(int incr) {
 		Indent.print("Orangutan addPoints()");
 		Indent.inc();
 		points++;
 		Indent.dec();
 	}
-
+	/**
+	* Torli az orangutan mogott allo sort.
+	* Meghivodik ExitDoor-ba lepes eseten.
+	**/
 	public void deleteQueue() {
 		Indent.print("Orangutan deleteQueue()");
 		Panda lastBackNeighbour = backNeighbour;
@@ -113,6 +172,10 @@ public class Orangutan extends Animal {
 			current.die();
 		}
 	}
+	/**
+	* Megragadja a parameterkent kapott pandat, es beallitja hatso szomszedjanak.
+	* @param p A panda, amit hatso szomszednak allit
+	**/
 	public void grab(Panda p) {
 		Indent.print("Orangutan grab(Panda)");
 		setBackNeighbour(p);
--- Spooky scary skeleton/Question.java
+++ Spooky scary skeleton/Question.java
@@ -5,11 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+* A kerdes osztaly, a szkeleton program magja.
+* Ez tartalmazza a kerdezo fuggvenyeket, melyekkel a szkeletont hasznalo
+* ki tudja valasztani, mit szeretne csinalni.
+* Fuggvenyei statikusak, hogy minden osztaly elerje oket, es ne
+* kelljen peldanyositani az osztalyt a hasznalatahoz.
+*
+* @author		Ferenczy Balint
+* @version 	1.0
+*/
+
 public class Question {
+	/**
+	* Csempe valasztasakor felmerulo kerdeseket kezeli,
+	* kivalaszthatjuk, milyen mezore akarunk lepni.
+	* @return		a kivalasztott csempe
+	**/
 	public static Tile selectTileQuestions() {
     	Indent.printr("Milyen csempere akarsz lepni? (T / BT / G)");
-		BufferedReader reader =  
-                new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
 		try {
 			String answer = reader.readLine();
 			answer = answer.toUpperCase();
@@ -19,19 +35,24 @@ public class Question {
 			case "G":  return new BreakableTile(0);
 			default:  return new Tile();
 			}
-			
+
 		} catch (IOException e) {
 			Indent.print("Valami szornyu valaszt adhattal meg, ezert a rendszer osszeomlott.");
 			e.printStackTrace();
-		} 
+		}
 		return new Tile();
     }
-	//Panda isInQueue reszenel hivodik meg
+	/**
+	* A Panda isInQueue() fuggvenyenel hivodik meg, a kerdesekkel beallithato,
+	* milyen pandak alljanak a panda mogott.
+	*
+	*	@param gp		A panda, ami moge a sort epitjuk
+	**/
     public static void queueQuestions(Panda gp) {
 		Indent.printr("All mogotted panda? (P / TP / SP / JP / N)");
 		ArrayList<Panda> pandasor = new ArrayList<>();
-		
-		BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in)); 
+
+		BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
 		try {
 			String answer = reader.readLine().toUpperCase();
 			while(!answer.equals("N")) {
@@ -53,29 +74,36 @@ public class Question {
 					pandasor.remove(0);
 				}
 			}
-			
+
 		} catch (IOException e) {
 			Indent.print("Valami szornyu valaszt adhattal meg, ezert a rendszer osszeomlott.");
 			e.printStackTrace();
-		} 
+		}
     }
+
+		/**
+		* Azok a kerdesek, amelyek egy csempe kivalasztasakor kerulnek elo.
+		* Itt valaszthatjuk ki, milyen csempere akarunk lepni, illetve,
+		*	hogy alljon-e mogottunk panda.
+		* @param a 		az allat, ami lepni akar
+		* @param gt		a csempe, amin az allat eddig allt
+		**/
 	public static void acceptQuestions(Animal a, Tile gt) {
 		Indent.printr("All valami a mezon, amire lepni szeretnel? (A / ED / XD / CM / GM / O / P / N / W)");
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			String answer = reader.readLine();
 			answer = answer.toUpperCase();
 			switch(answer) {
-			case "A": 
+			case "A":
 				Armchair ac = new Armchair();
 				gt.setElement(ac);
 				Indent.printr("Foglalt a fotel? (Y / N)");
 				if(reader.readLine().toUpperCase().equals("Y")) {
 					TiredPanda np = new TiredPanda();
 					np.setArmchair(ac);
-				} 
-				
+				}
 			break;
 			case "ED": gt.setElement(new EntryDoor());
 			break;
@@ -87,7 +115,7 @@ public class Question {
 			break;
 			case "O": gt.setElement(new Orangutan());
 			break;
-			case "P": 
+			case "P":
 				ArrayList<Panda> pandasor = new ArrayList<>();
 				Indent.printr("Milyen fajta panda? (P / TP / SP / JP)");
 				answer = reader.readLine().toUpperCase();
@@ -105,7 +133,7 @@ public class Question {
 
 					Indent.printr("All mogotte panda a lancban? (Y / N)");
 					answer = reader.readLine(); answer = answer.toUpperCase();
-					
+
 					String innerAnswer;
 					while(answer.equals("Y")) {
 						Indent.printr("Milyen panda all mogotte? (P / TP / SP / JP)");
@@ -128,28 +156,33 @@ public class Question {
 					pandasor.get(0).setFrontNeighbour(p.backNeighbour);
 					p = p.backNeighbour;
 					pandasor.remove(0);
-					
-					
+
+
 				}
-				
 				break;
 			case "W": gt.setElement(new Wardrobe());
 			break;
 			case "N": gt.setElement(null);
 			break;
-			default: gt.setElement(null);			
+			default: gt.setElement(null);
 			}
 
 		} catch (IOException e) {
 			Indent.print("Valami szornyu valaszt adhattal meg, ezert a rendszer osszeomlott.");
 			e.printStackTrace();
-		} 
+		}
 	}
+	/**
+	* Az orangutan sorban allasahoz tartozo kerdesek.
+	* Itt valaszthatjuk ki, milyen pandak alljanak az orang utan,
+	*	es mennyi legyen beloluk.
+	* @param go 		az orangutan, ami utan a sort epitjuk
+	**/
 public static void orangutanQueueQuestions(Orangutan go) {
 	Indent.printr("All mogotted panda? (P / TP / SP / JP / N)");
 	ArrayList<Panda> pandasor = new ArrayList<>();
-	
-	BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in)); 
+
+	BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
 	try {
 		String answer = reader.readLine().toUpperCase();
 		while(!answer.equals("N")) {
@@ -172,10 +205,10 @@ public static void orangutanQueueQuestions(Orangutan go) {
 				p = p.backNeighbour;
 			}
 		}
-		
+
 	} catch (IOException e) {
 		Indent.print("Valami szornyu valaszt adhattal meg, ezert a rendszer osszeomlott.");
 		e.printStackTrace();
-	} 
+	}
 }
 }
